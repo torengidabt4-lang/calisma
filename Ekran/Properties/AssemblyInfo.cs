8c7c27d8edb3198498367c8636b0363e4ea6dc11
torengidabt4-lang <torengidabt4@gmail.com>
2026-05-14 16:19:26 +0300

    Fix: Remove Unicode characters and unnecessary attributes from AssemblyInfo

--- Ekran/Properties/AssemblyInfo.cs
+++ Ekran/Properties/AssemblyInfo.cs
@@ -2,15 +2,14 @@ using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Ekran - Uzaktan Masaüstü")]
-[assembly: AssemblyDescription("Windows uzaktan masaüstü kontrol uygulaması")]
+[assembly: AssemblyTitle("Ekran")]
+[assembly: AssemblyDescription("Uzaktan Masaüstü Kontrol Uygulamasi")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Kişisel Proje")]
+[assembly: AssemblyCompany("Personal Project")]
 [assembly: AssemblyProduct("Ekran")]
-[assembly: AssemblyCopyright("Copyright © 2026")]
+[assembly: AssemblyCopyright("2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: NeutralResourcesLanguage("tr-TR")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
